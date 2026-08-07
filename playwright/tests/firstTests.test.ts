361b8c79b935af2f57a54b725a75b3615effebba
Frans <franslarsson@hotmail.com>
2026-08-07 15:00:22 +0200

    lägger till test

--- playwright/tests/firstTests.test.ts
+++ playwright/tests/firstTests.test.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('har en  titel', async ({ page }) => {
   await page.goto('https://demowebshop.tricentis.com/');
 
   // Expect a title "to contain" a substring.

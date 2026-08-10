7e52e96be0cd8976559aaffdee17469663b9115f
Frans <franslarsson@hotmail.com>
2026-08-10 10:20:07 +0200

    tar bort exempelfiler

--- playwright/tests/firstTests.test.ts
+++ playwright/tests/firstTests.test.ts
@@ -3,6 +3,7 @@ import { test, expect } from '@playwright/test';
 test('har en  titel', async ({ page }) => {
   await page.goto('https://demowebshop.tricentis.com/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Demo Web Shop/);
+  let title:string = await page.title();
+  console.log("Title = ", title);
+  await expect(page).toHaveTitle("Demo Web Shop");
 });

7125aa24cef6e3c10238cd1c631d1fbedb6671aa
Frans <franslarsson@hotmail.com>
2026-08-19 15:18:54 +0200

    fixar auth

--- playwright/tests/auth.setup.ts
+++ playwright/tests/auth.setup.ts
@@ -4,21 +4,11 @@ import path from 'path';
 const authFile = path.join(__dirname, '../.auth/user.json');
 
 setup('authenticate', async ({ page }) => {
-  // Perform authentication steps. Replace these actions with your own.
   await page.goto('/login');
   await page.getByLabel('Username').fill(process.env.USERNAME!);
   await page.getByLabel('Password').fill(process.env.PASSWORD!);
   await page.getByLabel('role').selectOption('Consumer');
   await page.getByRole('button', { name: 'Login' }).click();
-  // Wait until the page receives the cookies.
-  //
-  // Sometimes login flow sets cookies in the process of several redirects.
-  // Wait for the final URL to ensure that the cookies are actually set.
   await expect(page.getByRole('button', { name: 'Log Out' })).toBeVisible();
-  // Alternatively, you can wait until the page reaches a state where all cookies are set.
-  //await expect(page.getByRole('button', { name: 'View profile and more' })).toBeVisible();
-
-  // End of authentication steps.
-
   await page.context().storageState({ path: authFile });
 });

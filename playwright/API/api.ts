dbd17363f5af86052f90dac0e197d45f07adc959
Frans <franslarsson@hotmail.com>
2026-08-26 16:30:10 +0200

    fixar med api

--- playwright/API/api.ts
+++ playwright/API/api.ts
@@ -1,1 +1,22 @@
-import { APIRequestContext } from '@playwright/test';
+import { APIRequestContext } from '@playwright/test';   
+
+interface Student {
+  name: string;
+  age: string;
+  grade: string;
+}
+
+export class API {
+  constructor(private request: APIRequestContext) {}
+
+  async getStudents() {
+    const response = await this.request.get('/student');
+    return response;   
+}
+ async addStudent(studentData: Student) {
+    const response = await this.request.post('/student', { data: studentData });
+    return response;   
+}
+
+
+}
--- playwright/tests/api-tests/api.spec.ts
+++ playwright/tests/api-tests/api.spec.ts
@@ -1,10 +1,24 @@
 import { test, expect } from '@playwright/test';
+import { API } from '../../API/api';
+
+test('Lägger till en student', async ({ request}) => {
+    const api = new API(request);
+
+    const response = await api.addStudent({
+        name: 'Test Frasse',
+        age: '33',
+        grade: 'A',
+    });
+    expect(response.ok()).toBeTruthy();
+}); 
+
 
 
 test('hämtar studenter', async ({ request}) => {
-    const response = await request.get('/student');
-    const data = await response.json();
-    console.log(data);
+    const api = new API(request);
+
+    const response = await api.getStudents();
+    console.log(await response.json());
     expect(response.ok()).toBeTruthy();
  
     

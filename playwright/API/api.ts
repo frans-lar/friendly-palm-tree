c5352488ce468602317cd31c9f875fcae3427657
Frans <franslarsson@hotmail.com>
2026-08-27 16:12:52 +0200

    fixar med api

--- playwright/API/api.ts
+++ playwright/API/api.ts
@@ -6,16 +6,30 @@ interface Student {
   grade: string;
 }
 
+interface AddStudentResponse {
+  status: string;
+  student_id: number;
+}
+
+interface GetStudentsResponse {
+  id: number;
+  name: string;
+  age: string;
+  grade: string;
+  createdDate: string;
+  updatedDate: string;
+}
+
 export class API {
   constructor(private request: APIRequestContext) {}
 
-  async getStudents() {
+  async getStudents(): Promise<GetStudentsResponse[]> {
     const response = await this.request.get('/student');
-    return response;   
+    return await response.json();   
 }
- async addStudent(studentData: Student) {
+ async addStudent(studentData: Student): Promise<AddStudentResponse> {
     const response = await this.request.post('/student', { data: studentData });
-    return response;   
+    return await response.json();   
 }
 
 
--- playwright/tests/api-tests/api.spec.ts
+++ playwright/tests/api-tests/api.spec.ts
@@ -9,17 +9,23 @@ test('Lägger till en student', async ({ request}) => {
         age: '33',
         grade: 'A',
     });
-    expect(response.ok()).toBeTruthy();
+    const students = await api.getStudents();
+    const addedStudent = students.find(student => student.id === response.student_id);
+    expect(addedStudent).toBeDefined();
+    expect(addedStudent?.name).toBe('Test Frasse');
+    expect(addedStudent?.age).toBe('33');
+    expect(addedStudent?.grade).toBe('A');
 }); 
 
 
 
+/*
 test('hämtar studenter', async ({ request}) => {
     const api = new API(request);
 
-    const response = await api.getStudents();
-    console.log(await response.json());
-    expect(response.ok()).toBeTruthy();
- 
-    
+    const students = await api.getStudents();
+    expect(students.length).toBeGreaterThan(0);
+  
 });
+
+*/
